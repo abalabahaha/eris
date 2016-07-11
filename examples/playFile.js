6343300b573abde1679b782b896c8b61560d94d3
abalabahaha <hi@abal.moe>
2016-07-11 19:22:26 +0900

    More various fixes If the person in front of you in 744 economy reclines his/her seat all the way back, your 13" MBP will be left in an awkward barely usable position

---
+++ examples/playFile.js
@@ -0,0 +1,55 @@
+const Eris = require("eris");
+const FS = require("fs");
+
+var bot = new Eris("BOT_TOKEN");
+// Replace BOT_TOKEN with your bot account's token
+var playCommand = "!play";
+
+bot.on("ready", () => { // When the bot is ready
+    console.log("Ready!"); // Log "Ready!"
+});
+
+bot.on("messageCreate", (msg) => { // When a message is created
+    if(msg.content.startsWith(playCommand)) { // If the message content starts with "!play "
+        if(msg.content.length <= playCommand.length + 1) { // Check if a filename was specified
+            bot.createMessage(msg.channel.id, "Please specify a filename.");
+            return;
+        }
+        if(!msg.channel.guild) { // Check if the message was sent in a guild
+            bot.createMessage(msg.channel.id, "This command can only be run in a server.");
+            return;
+        }
+        if(!msg.member.channelID) { // Check if the user is in a voice channel
+            bot.createMessage(msg.channel.id, "You are not in a voice channel.");
+            return;
+        }
+        var filename = msg.content.substring(playCommand.length + 1); // Get the filename
+        FS.stat(filename, (err) => { // Try to read the file
+            if(err) {
+                if(err.code === "ENOENT") { // Handle ENOENT (file does not exist)
+                    bot.createMessage(msg.channel.id, "That file does not exist.");
+                    return;
+                } else { // Notify user about file access error
+                    bot.createMessage(msg.channel.id, "An error occured trying to access that file.");
+                    return;
+                }
+            } else {
+                bot.joinVoiceChannel(msg.member.channelID).catch((err) => { // Join the user's voice channel
+                    bot.createMessage(msg.channel.id, "Error joining voice channel: " + err.message); // Notify the user if there is an error
+                    console.log(err); // Log the error
+                }).then((connection) => {
+                    if(connection.playing) { // Stop playing if the connection is playing something
+                        connection.stopPlaying();
+                    }
+                    connection.playFile(filename); // Play the file and notify the user
+                    bot.createMessage(msg.channel.id, `Now playing **${filename}**`);
+                    connection.once("end", () => {
+                        bot.createMessage(msg.channel.id, `Finished **${filename}**`); // Say when the file has finished playing
+                    });
+                });
+            }
+        });
+    }
+});
+
+bot.connect(); // Get the bot to connect to Discord
