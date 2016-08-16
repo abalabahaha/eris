f785a1ddedc00a3f55c50335299eca22652634bb
abalabahaha <hi@abal.moe>
2016-08-17 06:33:07 +0900

    Tweaked opusscript

--- deps/opusscript/Opus.js
+++ deps/opusscript/Opus.js
@@ -15,10 +15,10 @@ const OpusError = {
     "-4": "Invalid packet",
     "-5": "Unimplemented",
     "-6": "Invalid state",
-    "-7": "Alloc fail"
+    "-7": "Memory allocation fail"
 };
 const VALID_SAMPLING_RATES = [8000, 12000, 16000, 24000, 48000];
-const MAX_FRAME_SIZE = 1276 * 3;
+const MAX_PACKET_SIZE = 1276 * 3;
 const SET_BITRATE_REQUEST = 4002;
 
 class OpusScript {
@@ -37,17 +37,19 @@ class OpusScript {
         this.application = application || OpusApplication.AUDIO;
         this.frameSize = this.samplingRate * this.frameDuration / 1000;
 
-        this.encoder = new opusscript_native.OpusScriptEncoder(this.samplingRate, this.channels, this.application);
+        this.handler = new opusscript_native.OpusScriptHandler(this.samplingRate, this.channels, this.application);
+
+        this.setBitrate(bitrate);
 
         this.inPCMLength = this.frameSize * this.channels * 2;
         this.inPCMPointer = opusscript_native._malloc(this.inPCMLength);
         this.inPCM = opusscript_native.HEAPU16.subarray(this.inPCMPointer, this.inPCMPointer + this.inPCMLength);
 
-        this.inOpusPointer = opusscript_native._malloc(MAX_FRAME_SIZE);
-        this.inOpus = opusscript_native.HEAPU8.subarray(this.inOpusPointer, this.inOpusPointer + MAX_FRAME_SIZE);
+        this.inOpusPointer = opusscript_native._malloc(MAX_PACKET_SIZE);
+        this.inOpus = opusscript_native.HEAPU8.subarray(this.inOpusPointer, this.inOpusPointer + MAX_PACKET_SIZE);
 
-        this.outOpusPointer = opusscript_native._malloc(MAX_FRAME_SIZE);
-        this.outOpus = opusscript_native.HEAPU8.subarray(this.outOpusPointer, this.outOpusPointer + MAX_FRAME_SIZE);
+        this.outOpusPointer = opusscript_native._malloc(MAX_PACKET_SIZE);
+        this.outOpus = opusscript_native.HEAPU8.subarray(this.outOpusPointer, this.outOpusPointer + MAX_PACKET_SIZE);
 
         this.outPCMLength = this.frameSize * this.channels * 2;
         this.outPCMPointer = opusscript_native._malloc(this.outPCMLength);
@@ -56,38 +58,38 @@ class OpusScript {
 
     setBitrate(bitrate) {
         this.bitrate = bitrate || 64000;
-        opus_native.setValue(this.bitratePointer, this.bitrate, "i32");
-        var errCode = opus_native._opus_encoder_ctl(this.encoder, SET_BITRATE_REQUEST, this.bitratePointer);
+        opusscript_native.setValue(this.bitratePointer, this.bitrate, "i32");
+        var errCode = opusscript_native._opus_encoder_ctl(this.handler, SET_BITRATE_REQUEST, this.bitratePointer);
         if(errCode < 0) {
-            throw new Error("Failed to set bitrate: " + OpusError["" + opus_native.getValue(errCode, "i32")]);
+            throw new Error("Failed to set bitrate: " + OpusError["" + opusscript_native.getValue(errCode, "i32")]);
         }
     }
 
-    encode(buffer) {
+    encode(buffer, frameSize) {
         if(buffer.length > this.inPCMLength) {
-            throw new RangeError("Buffer too large");
+            throw new RangeError("Buffer is too large");
         }
 
         this.inPCM.set(buffer);
 
-        var len = this.encoder._encode(this.inPCM.byteOffset, buffer.length, this.outOpusPointer, this.frameSize);
+        var len = this.handler._encode(this.inPCM.byteOffset, buffer.length, this.outOpusPointer, frameSize);
         if(len < 0) {
-            throw new Error("Encode error: " + OpusError["" + opus_native.getValue(len, "i32")]);
+            throw new Error("Encode error: " + OpusError["" + opusscript_native.getValue(len, "i32")]);
         }
 
         return new Buffer(this.outOpus.subarray(0, len));
     }
 
     decode(buffer) {
-        if(buffer.length > MAX_FRAME_SIZE) {
-            throw new RangeError("Buffer too large");
+        if(buffer.length > MAX_PACKET_SIZE) {
+            throw new RangeError("Buffer is too large");
         }
 
         this.inOpus.set(buffer);
 
-        var len = this.encoder._decode(this.inOpusPointer, this.outPCM.byteOffset, buffer.length);
+        var len = this.handler._decode(this.inOpusPointer, buffer.length, this.outPCM.byteOffset);
         if(len < 0) {
-            throw new Error("Decode error: " + OpusError["" + opus_native.getValue(len, "i32")]);
+            throw new Error("Decode error: " + OpusError["" + len]);
         }
 
         return new Buffer(this.outPCM.subarray(0, len * this.channels * 2));
@@ -97,6 +99,6 @@ class OpusScript {
 OpusScript.Application = OpusApplication;
 OpusScript.Error = OpusError;
 OpusScript.VALID_SAMPLING_RATES = VALID_SAMPLING_RATES;
-OpusScript.MAX_FRAME_SIZE = MAX_FRAME_SIZE;
+OpusScript.MAX_PACKET_SIZE = MAX_PACKET_SIZE;
 
 module.exports = OpusScript;
