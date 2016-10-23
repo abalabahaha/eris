4ca470b3d4c79792b9848aa6d2463e4f7ea59c2f
abalabahaha <hi@abal.moe>
2016-10-24 03:39:45 +0900

    Simplify SharedStream

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ Eris.PrivateChannel = require("./lib/structures/PrivateChannel");
 Eris.Relationship = require("./lib/structures/Relationship");
 Eris.Role = require("./lib/structures/Role");
 Eris.Shard = require("./lib/gateway/Shard");
+Eris.SharedStream = require("./lib/voice/SharedStream");
 Eris.User = require("./lib/structures/User");
 Eris.VoiceConnection = require("./lib/voice/VoiceConnection");
 Eris.VoiceConnectionManager = require("./lib/voice/VoiceConnectionManager");
