e6417e0372898f5daba88f6fd61971962f64674f
Brayzure <rictylp@gmail.com>
2017-01-25 01:25:22 -0500

    Requested Changes -Move embed property to new line

--- examples/embed.js
+++ examples/embed.js
@@ -9,30 +9,32 @@ bot.on("ready", () => { // When the bot is ready
 
 bot.on("messageCreate", (msg) => { // When a message is created
     if(msg.content === "!embed") { // If the message content is "!embed"
-        bot.createMessage(msg.channel.id, {embed: {
-            title: "I'm an embed!", // Title of the embed
-            description: "Here is some more info, with **awesome** formatting.\nPretty *neat*, huh?",
-            author: { // Author property
-                name: msg.author.username,
-                icon_url: msg.author.avatarURL
-            },
-            color: 0x008000, // Color, either in hex (show), or a base-10 integer
-            fields: [ // Array of field objects
-                {
-                    name: "Some extra info.", // Field title
-                    value: "Some extra value.", // Field
-                    inline: true // Whether you want multiple fields in same line
+        bot.createMessage(msg.channel.id, {
+            embed: {
+                title: "I'm an embed!", // Title of the embed
+                description: "Here is some more info, with **awesome** formatting.\nPretty *neat*, huh?",
+                author: { // Author property
+                    name: msg.author.username,
+                    icon_url: msg.author.avatarURL
                 },
-                {
-                    name: "Some more extra info.",
-                    value: "Another extra value.",
-                    inline: true
+                color: 0x008000, // Color, either in hex (show), or a base-10 integer
+                fields: [ // Array of field objects
+                    {
+                        name: "Some extra info.", // Field title
+                        value: "Some extra value.", // Field
+                        inline: true // Whether you want multiple fields in same line
+                    },
+                    {
+                        name: "Some more extra info.",
+                        value: "Another extra value.",
+                        inline: true
+                    }
+                ],
+                footer: { // Footer text
+                    text: "Created with Eris."
                 }
-            ],
-            footer: { // Footer text
-                text: "Created with Eris."
             }
-        }});
+        });
     }
 });
 
