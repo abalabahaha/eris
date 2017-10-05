aff19ab211ee52fa9725be3bb525347747a1e933
Aaron Scherer <aequasi@gmail.com>
2017-10-05 12:54:02 -0700

    Optional RoleOption params

--- index.d.ts
+++ index.d.ts
@@ -155,7 +155,7 @@ declare module "eris" {
     splash?: string;
   }
   interface MemberOptions { roles: string[]; nick: string; mute: boolean; deaf: boolean; channelID: string; }
-  interface RoleOptions { name: string; permissions: number; color: number; hoist: boolean; mentionable: boolean; }
+  interface RoleOptions { name?: string; permissions?: number; color?: number; hoist?: boolean; mentionable?: boolean; }
   interface GamePresence { name: string; type?: number; url?: string; }
   interface SearchOptions {
     sortBy?: string;
