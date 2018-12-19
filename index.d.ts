96890094deb9975509f85fee517e1c334f56e090
George <georgej1088@gmail.com>
2018-12-19 11:25:31 -0500

    TS: Sort function is technically optional
    I don't know whether or not this is useful but it's technically right

--- index.d.ts
+++ index.d.ts
@@ -957,7 +957,7 @@ declare module "eris" {
     public filter(func: (i: T) => boolean): T[];
     public map<R>(func: (i: T) => R): R[];
     public reduce<U>(func: (accumulator: U, val: T) => U, initial?: U): U;
-    public sort(func: (a: T, b: T) => number): T[];
+    public sort(func?: (a: T, b: T) => number): T[];
     public every(func: (i: T) => boolean): boolean;
     public some(func: (i: T) => boolean): boolean;
     public update(obj: T, extra?: any, replace?: boolean): T;
