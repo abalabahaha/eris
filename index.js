4c5163f357f8410a8869405254b7c42d5bb28043
August (Chris) <augu@voided.pw>
2019-02-07 18:51:37 -0700

    owo

--- index.js
+++ index.js
@@ -2,45 +2,46 @@
 
 const Client = require("./lib/Client");
 
-function Eris(token, options) {
-    return new Client(token, options);
-}
-
-Eris.Base = require("./lib/structures/Base");
-Eris.Bucket = require("./lib/util/Bucket");
-Eris.Call = require("./lib/structures/Call");
-Eris.CategoryChannel = require("./lib/structures/CategoryChannel");
-Eris.Channel = require("./lib/structures/Channel");
-Eris.Client = Client;
-Eris.Collection = require("./lib/util/Collection");
-Eris.Command = require("./lib/command/Command");
-Eris.CommandClient = require("./lib/command/CommandClient");
-Eris.Constants = require("./lib/Constants");
-Eris.ExtendedUser = require("./lib/structures/ExtendedUser");
-Eris.GroupChannel = require("./lib/structures/GroupChannel");
-Eris.Guild = require("./lib/structures/Guild");
-Eris.GuildChannel = require("./lib/structures/GuildChannel");
-Eris.GuildIntegration = require("./lib/structures/GuildIntegration");
-Eris.Invite = require("./lib/structures/Invite");
-Eris.Member = require("./lib/structures/Member");
-Eris.Message = require("./lib/structures/Message");
-Eris.Permission = require("./lib/structures/Permission");
-Eris.PermissionOverwrite = require("./lib/structures/PermissionOverwrite");
-Eris.PrivateChannel = require("./lib/structures/PrivateChannel");
-Eris.Relationship = require("./lib/structures/Relationship");
-Eris.Role = require("./lib/structures/Role");
-Eris.Shard = require("./lib/gateway/Shard");
-Eris.SharedStream = require("./lib/voice/SharedStream");
-Eris.TextChannel = require("./lib/structures/TextChannel");
-Eris.User = require("./lib/structures/User");
-Eris.VERSION = require('./package.json').version;
-Eris.VoiceChannel = require("./lib/structures/VoiceChannel");
-Eris.VoiceConnection = require("./lib/voice/VoiceConnection");
-Eris.VoiceConnectionManager = require("./lib/voice/VoiceConnectionManager");
-Eris.VoiceState = require("./lib/structures/VoiceState");
-
-Object.keys(Eris).filter(prop => Eris.hasOwnProperty(prop) && typeof Eris[prop] === "function" && !(Eris[prop] instanceof Eris.Base)).forEach(prop => {
-    Eris[prop].prototype.toString = Eris.Base.prototype.toString;
-});
-
-module.exports = Eris;
+module.exports = {
+    /**
+     * Create a new `Client` instance
+     * @param {string} token The token to the bot
+     * @param {import('./lib/Client').ClientOptions} [options] Any other options to addon to
+     */
+    create: (token, options = {}) => new Client(token, options),
+    Base: require("./lib/structures/Base"),
+    Bucket: require("./lib/util/Bucket"),
+    Call: require("./lib/structures/Call"),
+    CategoryChannel: require("./lib/structures/CategoryChannel"),
+    Channel: require("./lib/structures/Channel"),
+    Client,
+    Collection: require("./lib/util/Collection"),
+    Constants: require("./lib/Constants"),
+    EmbedUtil: require("./lib/util/EmbedUtil"),
+    Emoji: require("./lib/structures/Emoji"),
+    ExtendedUser: require("./lib/structures/ExtendedUser"),
+    GroupChannel: require("./lib/structures/GroupChannel"),
+    Guild: require("./lib/structures/Guild"),
+    GuildChannel: require("./lib/structures/GuildChannel"),
+    GuildIntegration: require("./lib/structures/GuildIntegration"),
+    Invite: require("./lib/structures/Invite"),
+    Member: require("./lib/structures/Member"),
+    Message: require("./lib/structures/Message"),
+    MessageCollector: require("./lib/structures/MessageCollector"),
+    MessageEmbed: require("./lib/structures/MessageEmbed"),
+    Permission: require("./lib/structures/Permission"),
+    PermissionOverwrite: require("./lib/structures/PermissionOverwrite"),
+    PrivateChannel: require("./lib/structures/PrivateChannel"),
+    Relationship: require("./lib/structures/Relationship"),
+    Role: require("./lib/structures/Role"),
+    Shard: require("./lib/gateway/Shard"),
+    SharedStream: require("./lib/voice/SharedStream"),
+    TextChannel: require("./lib/structures/TextChannel"),
+    User: require("./lib/structures/User"),
+    version: require("./package").version,
+    VoiceChannel: require("./lib/structures/VoiceChannel"),
+    VoiceConnection: require("./lib/voice/VoiceConnection"),
+    VoiceConnectionManager: require("./lib/voice/VoiceConnectionManager"),
+    VoiceState: require("./lib/structures/VoiceState"),
+    WebSocketManager: require("./lib/gateway/WebSocketManager")
+};
