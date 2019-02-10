ea44550dbbf740b5f094ca56897db1ebbe796e41
George <georgej1088@gmail.com>
2019-02-10 08:30:04 -0500

    Add, improve Collection methods (#442)
    * Add every(), reduce(), sum() to Collection
    
    * Faster random() implementation

--- index.d.ts
+++ index.d.ts
@@ -957,6 +957,9 @@ declare module "eris" {
     public random(): T;
     public filter(func: (i: T) => boolean): T[];
     public map<R>(func: (i: T) => R): R[];
+    public reduce<U>(func: (accumulator: U, val: T) => U, initial?: U): U;
+    public every(func: (i: T) => boolean): boolean;
+    public some(func: (i: T) => boolean): boolean;
     public update(obj: T, extra?: any, replace?: boolean): T;
     public remove(obj: T | { id: string }): T;
   }
