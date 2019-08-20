5bb9fe099382f7429e54ae80e6a19c66fd3ad4aa
Yegor Gunko <43087973+yegorgunko@users.noreply.github.com>
2019-08-20 11:30:59 +0300

    Fixed return value for registerCommand

--- index.d.ts
+++ index.d.ts
@@ -1606,7 +1606,7 @@ declare module "eris" {
     public hidden: boolean;
     public constructor(label: string, generate: CommandGenerator, options?: CommandOptions);
     public registerSubcommandAlias(alias: string, label: string): void;
-    public registerSubcommand(label: string, generator: CommandGenerator, options?: CommandOptions): void;
+    public registerSubcommand(label: string, generator: CommandGenerator, options?: CommandOptions): Command;
     public unregisterSubcommand(label: string): void;
   }
 
