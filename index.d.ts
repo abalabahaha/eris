5febc74f3035a59ba3739a248431813efe121b1c
Yegor Gunko <43087973+yegorgunko@users.noreply.github.com>
2019-09-10 02:25:38 +0300

    Added lastPinTimestamp to TextChannel typings (#525)

--- index.d.ts
+++ index.d.ts
@@ -1158,6 +1158,7 @@ declare module "eris" {
     public lastMessageID: string;
     public rateLimitPerUser: number;
     public messages: Collection<Message>;
+    public lastPinTimestamp?: number;
     public constructor(data: BaseData, guild: Guild, messageLimit: number);
     public getInvites(): Promise<Invite[]>;
     public createInvite(options?: CreateInviteOptions, reason?: string): Promise<Invite>;
