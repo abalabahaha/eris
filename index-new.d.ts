7f26f3d8406ef651de53dee07badfebb7769a606
George <georgej1088@gmail.com>
2019-09-19 14:24:20 -0400

    Add basic Collection class + supporting interfaces

--- index-new.d.ts
+++ index-new.d.ts
@@ -6,4 +6,29 @@ declare module "eris" {
 		toString(): string;
 		toJSON(simple?: boolean): object;
 	}
+
+	/** A data object that can be constructed into a structure */
+	interface Collectable {
+		id: string; // TODO: this was previously `string | number` but I don't believe there are any instances of numbers being used as IDs in the code
+	}
+
+	/** A constructor that can be used as the baseObject of a Collection */
+	interface BaseObject<T extends Collectable> {
+		/** Takes a data object and an optional extra parameter */
+		new (obj: Collectable, ...extra: any[]): T;
+	}
+
+	class Collection<T extends Collectable> extends Map<string, T> {
+		readonly baseObject: BaseObject<T>;
+		readonly limit: number;
+		constructor(baseObject: BaseObject<T>, limit?: number);
+		add(obj: Collectable, extra?: any, replace?: boolean): T;
+		find(func: (obj: T) => boolean): T | undefined;
+		random(): T | undefined;
+		filter(func: (obj: T) => boolean): T[];
+		map<U>(func: (obj: T) => U): U[];
+		update(obj: Collectable, extra?: any, replace?: boolean): T;
+		remove(obj: Collectable): T | null;
+		toString(): string;
+	}
 }
