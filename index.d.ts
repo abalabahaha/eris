069204171c6b3e1275e3e71edda1596e0ce156a5
Skillz <gankstarsleague@gmail.com>
2019-10-27 14:02:40 -0400

    channelMentions is never undefined

--- index.d.ts
+++ index.d.ts
@@ -1315,7 +1315,7 @@ declare module "eris" {
     public content: string;
     public cleanContent?: string;
     public roleMentions: string[];
-    public channelMentions?: string[];
+    public channelMentions: string[];
     public editedTimestamp?: number;
     public tts: boolean;
     public mentionEveryone: boolean;
