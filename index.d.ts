dbb175971e98f077f2dbcad6178e93e8ebc34054
Donovan Daniels <dwddaniels262@gmail.com>
2019-11-13 12:54:25 -0600

    Return a Message object when wait is true on executeWebhook
    When setting `wait` to true, it should say that it returns a Message object, like it does. This can be accomplished via an intersection (&)
    ```typescript
    Interface & { overrideProp: value; }
    ```
    just makes it return a different type whenever that property is that specific value, ex:
    ```typescript
    interface Payload {
    	id?: string;
    	wait?: boolean;
    }
    
    function j(s: string, j: string, o: Payload & { wait: true }): Promise<null>;
    function j(s: string, j: string, o: Payload & { wait?: false }): Promise<void>;
    function j(s: string, j: string, o: Payload): Promise<undefined> {
    	return;
    }
    
    j(null, null, { wait: true }); // null
    j(null, null, { wait: false }); // void
    j(null, null, {}); // void
    ```
    [true (first)](https://butts-are.cool/Code_-_Insiders_NMbyVdjHia.png)
    [false (second)](https://butts-are.cool/Code_-_Insiders_PeemMiJEVS.png)
    [none (third)](https://butts-are.cool/Code_-_Insiders_lXg92MjFgQ.png)

--- index.d.ts
+++ index.d.ts
@@ -643,7 +643,8 @@ declare namespace Eris {
       token?: string,
       reason?: string,
     ): Promise<Webhook>;
-    public executeWebhook(webhookID: string, token: string, options: WebhookPayload): Promise<void>;
+    public executeWebhook(webhookID: string, token: string, options: WebhookPayload & { wait: true; }): Promise<Eris.Message>;
+    public executeWebhook(webhookID: string, token: string, options: WebhookPayload & { wait?: false; ): Promise<void>;
     public executeSlackWebhook(webhookID: string, token: string, options?: { wait?: boolean }): Promise<void>;
     public deleteWebhook(webhookID: string, token?: string, reason?: string): Promise<void>;
     public getGuildWebhooks(guildID: string): Promise<Webhook[]>;
