e1cb0a21a5ad392e17b8c24c66e356be0e595a20
Donovan Daniels <dwddaniels262@gmail.com>
2019-11-13 16:13:33 -0600

    "Message" instead of "Eris.Message"

--- index.d.ts
+++ index.d.ts
@@ -643,7 +643,7 @@ declare namespace Eris {
       token?: string,
       reason?: string,
     ): Promise<Webhook>;
-    public executeWebhook(webhookID: string, token: string, options: WebhookPayload & { wait: true; }): Promise<Eris.Message>;
+    public executeWebhook(webhookID: string, token: string, options: WebhookPayload & { wait: true; }): Promise<Message>;
     public executeWebhook(webhookID: string, token: string, options: WebhookPayload & { wait?: false; ): Promise<void>;
     public executeSlackWebhook(webhookID: string, token: string, options?: { wait?: boolean }): Promise<void>;
     public deleteWebhook(webhookID: string, token?: string, reason?: string): Promise<void>;
