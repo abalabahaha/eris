5c59f1789baec69ab3976ca6815dd39a4ca18a0b
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2019-11-25 17:55:21 -0500

    Allow kicking voice channel members in typings (#573)

--- index.d.ts
+++ index.d.ts
@@ -538,7 +538,7 @@ declare namespace Eris {
     nick?: string;
     mute?: boolean;
     deaf?: boolean;
-    channelID?: string;
+    channelID?: string | null;
   }
   interface RoleOptions {
     name?: string;
