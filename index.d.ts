fc7ef7e954068367f6ba6852b987a0f52488c71c
Donovan_DMC <dwddaniels262@gmail.com>
2020-01-24 12:12:19 -0600

    explicitly type shard status

--- index.d.ts
+++ index.d.ts
@@ -1777,7 +1777,7 @@ declare namespace Eris {
     connecting: boolean;
     ready: boolean;
     discordServerTrace?: string[];
-    status: string;
+    status: "disconnected" | "connecting" | "handshaking" | "ready" | "resuming";
     lastHeartbeatReceived: number;
     lastHeartbeatSent: number;
     latency: number;
