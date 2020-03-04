abalabahaha/eris

commit 2fdf6c02b89a17ffef6532682f1e42bcc3455fc9

Support message type 14/15 (#642)

lib/structures/Message.js
@@ -153,6 +153,14 @@ class Message extends Base {
                 data.content = `${this.author.mention} has added ${data.content} to this channel`;
                 break;
             }
+            case MessageTypes.GUILD_DISCOVERY_DISQUALIFIED: {
+                data.content = 'This server has been removed from Server Discovery because it no longer passes all the requirements. Check `Server Settings` for more details.';
+                break;
+            }
+            case MessageTypes.GUILD_DISCOVERY_REQUALIFIED: {
+                data.content = 'This server is eligible for Server Discovery again and has been automatically relisted!';
+                break;
+            }
             default: {
                 client.emit("warn", `Unhandled MESSAGE_CREATE type: ${JSON.stringify(data, null, 2)}`);
                 break;

index.d.ts
@@ -347,6 +347,9 @@ declare namespace Eris {
       USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_2: 10;
       USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_3: 11;
       CHANNEL_FOLLOW_ADD: 12;
+
+      GUILD_DISCOVERY_DISQUALIFIED: 14;
+      GUILD_DISCOVERY_REQUALIFIED: 15;
     };
     ChannelTypes: {
       GUILD_TEXT: 0;

lib/Constants.js
@@ -189,7 +189,10 @@ module.exports.MessageTypes = {
     USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_1: 9,
     USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_2: 10,
     USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_3: 11,
-    CHANNEL_FOLLOW_ADD: 12
+    CHANNEL_FOLLOW_ADD: 12,
+
+    GUILD_DISCOVERY_DISQUALIFIED: 14,
+    GUILD_DISCOVERY_REQUALIFIED: 15
 };
 
 module.exports.ChannelTypes = {

commit 9332a675051ec759f8a26a3429964ab35e5f5842

Fix linting error. Lint error introduced in 2fdf6c02b89a17ffef6532682f1e42bcc3455fc9

lib/structures/Message.js
@@ -154,11 +154,11 @@ class Message extends Base {
                 break;
             }
             case MessageTypes.GUILD_DISCOVERY_DISQUALIFIED: {
-                data.content = 'This server has been removed from Server Discovery because it no longer passes all the requirements. Check `Server Settings` for more details.';
+                data.content = "This server has been removed from Server Discovery because it no longer passes all the requirements. Check `Server Settings` for more details.";
                 break;
             }
             case MessageTypes.GUILD_DISCOVERY_REQUALIFIED: {
-                data.content = 'This server is eligible for Server Discovery again and has been automatically relisted!';
+                data.content = "This server is eligible for Server Discovery again and has been automatically relisted!";
                 break;
             }
             default: {