b9d6de14e3997dc24a4e49f9ad9cfb9cd61a0a4e
abalabahaha <hi@abal.moe>
2020-08-19 09:45:30 -0700

    Add typings for PossiblyUncachedMessage#guildID

--- index.d.ts
+++ index.d.ts
@@ -715,7 +715,7 @@ declare namespace Eris {
     frameSize?: number;
     sampleRate?: number;
   }
-  type PossiblyUncachedMessage = Message | { id: string; channel: TextableChannel | { id: string; guild: { id: string } } };
+  type PossiblyUncachedMessage = Message | { id: string; channel: TextableChannel | { id: string; guild: { id: string } }; guildID: string };
   interface RawPacket {
     op: number;
     t?: string;
