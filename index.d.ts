ea4d079d8afe5bc859995c71ede7806b3b307bc1
Gleb <38599423+Gleb-ko@users.noreply.github.com>
2021-03-24 19:19:12 +0000

    Added typings for attachment image dimensions (#1112)

--- index.d.ts
+++ index.d.ts
@@ -714,6 +714,8 @@ declare namespace Eris {
     proxy_url: string;
     size: number;
     url: string;
+    height?: number;
+    width?: number;
   }
   interface MessageActivity {
     party_id?: string;
