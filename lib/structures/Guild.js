abalabahaha/eris

commit b6d51ee48600b94ec26b3ff7f043327eb0713101

Add REST methods for stage channels (#1155)

lib/structures/Guild.js
@@ -338,7 +338,7 @@ class Guild extends Base {
     /**
     * Create a channel in the guild
     * @arg {String} name The name of the channel
-    * @arg {Number} [type=0] The type of the channel, either 0 (text), 2 (voice), or 4 (category)
+    * @arg {Number} [type=0] The type of the channel, either 0 (text), 2 (voice), 4 (category), 5 (news) or 13 (stage)
     * @arg {Object | String} [options] The properties the channel should have. If `options` is a string, it will be treated as `options.parentID` (see below). Passing a string is deprecated and will not be supported in future versions.
     * @arg {Number} [options.bitrate] The bitrate of the channel (voice channels only)
     * @arg {Boolean} [options.nsfw] The nsfw status of the channel
@@ -605,13 +605,26 @@ class Guild extends Base {
         return this._client.editGuildTemplate.call(this._client, this.id, code, options);
     }
 
-    /**
+    /*
     * Modify a guild's vanity code
     * @arg {String} code The new vanity code
     * @returns {Promise<Object>}
     */
     editVanity(code) {
         return this._client.editGuildVanity.call(this._client, this.id, code);
+    }
+
+    /**
+    * Update a user's voice state - See [caveats](https://discord.com/developers/docs/resources/guild#update-others-voice-state-caveats)
+    * @param {Object} options The properties to edit
+    * @param {String} options.channelID The ID of the channel the user is currently in
+    * @param {Date?} [options.requestToSpeakTimestamp] Sets the user's request to speak - this can only be used when the `userID` param is "@me"
+    * @param {Boolean} [options.suppress] Toggles the user's suppress state
+    * @param {String} [userID="@me"] The user ID of the user to update
+    * @returns {Promise}
+    */
+    editVoiceState(options, userID = "@me") {
+        return this._client.editGuildVoiceState.call(this._client, this.id, options, userID);
     }
 
     /**

index.d.ts
@@ -903,6 +903,11 @@ declare namespace Eris {
     guild_id: string;
     shard: Shard;
   }
+  interface VoiceStateOptions {
+    channelID: string;
+    requestToSpeakTimestamp?: Date | null;
+    suppress?: boolean;
+  }
   interface VoiceStreamCurrent {
     options: VoiceResourceOptions;
     pausedTime?: number;
@@ -1420,6 +1425,12 @@ declare namespace Eris {
     createChannel(
       guildID: string,
       name: string,
+      type: 13,
+      options?: CreateChannelOptions
+    ): Promise<StageChannel>;
+    createChannel(
+      guildID: string,
+      name: string,
       type?: number,
       options?: CreateChannelOptions
     ): Promise<unknown>;
@@ -1463,6 +1474,14 @@ declare namespace Eris {
       reason?: string,
       options?: CreateChannelOptions | string
     ): Promise<StoreChannel>;
+    /** @deprecated */
+    createChannel(
+      guildID: string,
+      name: string,
+      type: 13,
+      reason?: string,
+      options?: CreateChannelOptions | string
+    ): Promise<StageChannel>;
     /** @deprecated */
     createChannel(
       guildID: string,
@@ -1534,6 +1553,7 @@ declare namespace Eris {
     editGuildMember(guildID: string, memberID: string, options: MemberOptions, reason?: string): Promise<void>;
     editGuildTemplate(guildID: string, code: string, options: GuildTemplateOptions): Promise<GuildTemplate>;
     editGuildVanity(guildID: string, code: string): Promise<GuildVanity>;
+    editGuildVoiceState(guildID: string, options: VoiceStateOptions, userID?: string): Promise<void>;
     editGuildWidget(guildID: string, options: Widget): Promise<Widget>
     editMessage(channelID: string, messageID: string, content: MessageContent): Promise<Message>;
     editNickname(guildID: string, nick: string, reason?: string): Promise<void>;
@@ -1875,6 +1895,7 @@ declare namespace Eris {
     createChannel(name: string, type: 4, options?: CreateChannelOptions): Promise<CategoryChannel>;
     createChannel(name: string, type: 5, options?: CreateChannelOptions | string): Promise<NewsChannel>;
     createChannel(name: string, type: 6, options?: CreateChannelOptions | string): Promise<StoreChannel>;
+    createChannel(name: string, type: 13, options?: CreateChannelOptions | string): Promise<StageChannel>;
     createChannel(name: string, type?: number, options?: CreateChannelOptions): Promise<unknown>;
     /** @deprecated */
     createChannel(name: string, type: 0, reason?: string, options?: CreateChannelOptions | string): Promise<TextChannel>;
@@ -1886,6 +1907,8 @@ declare namespace Eris {
     createChannel(name: string, type: 5, reason?: string, options?: CreateChannelOptions | string): Promise<NewsChannel>;
     /** @deprecated */
     createChannel(name: string, type: 6, reason?: string, options?: CreateChannelOptions | string): Promise<StoreChannel>;
+    /** @deprecated */
+    createChannel(name: string, type: 13, reason?: string, options?: CreateChannelOptions | string): Promise<StageChannel>;
     /** @deprecated */
     createChannel(name: string, type?: number, reason?: string, options?: CreateChannelOptions | string): Promise<unknown>;
     createEmoji(options: { image: string; name: string; roles?: string[] }, reason?: string): Promise<Emoji>;
@@ -1908,8 +1931,9 @@ declare namespace Eris {
     editMember(memberID: string, options: MemberOptions, reason?: string): Promise<void>;
     editNickname(nick: string): Promise<void>;
     editRole(roleID: string, options: RoleOptions): Promise<Role>;
-    editTemplate(code: string, options: GuildTemplateOptions): Promise<GuildTemplate>
+    editTemplate(code: string, options: GuildTemplateOptions): Promise<GuildTemplate>;
     editVanity(code: string): Promise<GuildVanity>;
+    editVoiceState(options: VoiceStateOptions, userID?: string): Promise<void>;
     editWidget(options: Widget): Promise<Widget>;
     fetchAllMembers(timeout?: number): Promise<number>;
     fetchMembers(options?: FetchMembersOptions): Promise<Member[]>;

lib/Client.js
@@ -403,7 +403,7 @@ class Client extends EventEmitter {
     * Create a channel in a guild
     * @arg {String} guildID The ID of the guild to create the channel in
     * @arg {String} name The name of the channel
-    * @arg {String} [type=0] The type of the channel, either 0 (text), 2 (voice), or 4 (category)
+    * @arg {String} [type=0] The type of the channel, either 0 (text), 2 (voice), 4 (category), 5 (news), 6 (store), or 13 (stage)
     * @arg {Object | String} [options] The properties the channel should have. If `options` is a string, it will be treated as `options.parentID` (see below). Passing a string is deprecated and will not be supported in future versions.
     * @arg {Number} [options.bitrate] The bitrate of the channel (voice channels only)
     * @arg {Boolean} [options.nsfw] The nsfw status of the channel
@@ -1120,6 +1120,24 @@ class Client extends EventEmitter {
     */
     editGuildTemplate(guildID, code, options) {
         return this.requestHandler.request("PATCH", Endpoints.GUILD_TEMPLATE_GUILD(guildID, code), true, options).then((template) => new GuildTemplate(template, this));
+    }
+
+    /**
+    * Update a user's voice state - See [caveats](https://discord.com/developers/docs/resources/guild#update-others-voice-state-caveats)
+    * @param {String} guildID The ID of the guild
+    * @param {Object} options The properties to edit
+    * @param {String} options.channelID The ID of the channel the user is currently in
+    * @param {Date?} [options.requestToSpeakTimestamp] Sets the user's request to speak - this can only be used when the `userID` param is "@me"
+    * @param {Boolean} [options.suppress] Toggles the user's suppress state
+    * @param {String} [userID="@me"] The user ID of the user to update
+    * @returns {Promise}
+    */
+    editGuildVoiceState(guildID, options, userID = "@me") {
+        return this.requestHandler.request("PATCH", Endpoints.GUILD_VOICE_STATE(guildID, userID), true, {
+            channel_id: options.channelID,
+            request_to_speak_timestamp: options.requestToSpeakTimestamp,
+            suppress: options.suppress
+        });
     }
 
     /**

lib/rest/Endpoints.js
@@ -62,6 +62,7 @@ module.exports.GUILD_TEMPLATE_GUILD =                            (guildID, code)
 module.exports.GUILD_VOICE_REGIONS =                                   (guildID) => `/guilds/${guildID}/regions`;
 module.exports.GUILD_WEBHOOKS =                                        (guildID) => `/guilds/${guildID}/webhooks`;
 module.exports.GUILD_WIDGET =                                          (guildID) => `/guilds/${guildID}/widget`;
+module.exports.GUILD_VOICE_STATE =                               (guildID, user) => `/guilds/${guildID}/voice-states/${user}`;
 module.exports.GUILDS =                                                             "/guilds";
 module.exports.INVITE =                                               (inviteID) => `/invite/${inviteID}`;
 module.exports.OAUTH2_APPLICATION =                                      (appID) => `/oauth2/applications/${appID}`;

commit b3c1b38fac6def8220b092caf7595fad2a4a94ea

Fix JSDoc comment
Broken by b6d51ee (#1155)

lib/structures/Guild.js
@@ -605,7 +605,7 @@ class Guild extends Base {
         return this._client.editGuildTemplate.call(this._client, this.id, code, options);
     }
 
-    /*
+    /**
     * Modify a guild's vanity code
     * @arg {String} code The new vanity code
     * @returns {Promise<Object>}