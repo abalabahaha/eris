c3d491d4d7a786a1b2fb5a2190d6334eec3e23bd
bsian03 <chharry321@gmail.com>
2021-04-25 16:48:37 +0100

    Move to content.messageReference (#1187)
    Co-authored-by: abalabahaha <hi@abal.moe>

--- index.d.ts
+++ index.d.ts
@@ -48,6 +48,8 @@ declare namespace Eris {
     content?: string;
     embed?: EmbedOptions;
     flags?: number;
+    messageReference?: MessageReferenceReply;
+    /** @deprecated */
     messageReferenceID?: string;
     tts?: boolean;
   };
@@ -803,6 +805,10 @@ declare namespace Eris {
     channelID?: string;
     guildID?: string;
     messageID?: string;
+  }
+  interface MessageReferenceReply extends MessageReferenceBase {
+    messageID: string;
+    failIfNotExists?: boolean;
   }
   interface Sticker {
     asset: string;
