0d352e861be91e23e8b8053b881677631bdcfab9
5antos <a67984@ualg.pt>
2021-05-03 22:36:18 +0100

    Enhancements for VoiceChannel join methods

--- index.d.ts
+++ index.d.ts
@@ -1756,7 +1756,7 @@ declare namespace Eris {
     getVoiceRegions(guildID?: string): Promise<VoiceRegion[]>;
     getWebhook(webhookID: string, token?: string): Promise<Webhook>;
     getWebhookMessage(webhookID: string, token: string, messageID: string): Promise<Message<GuildTextableChannel>>;
-    joinVoiceChannel(channelID: string, options?: { opusOnly?: boolean; shared?: boolean }): Promise<VoiceConnection>;
+    joinVoiceChannel(channelID: string, options?: { opusOnly?: boolean; shared?: boolean, selfMute?: boolean, selfDeaf?: boolean }): Promise<VoiceConnection>;
     kickGuildMember(guildID: string, userID: string, reason?: string): Promise<void>;
     leaveGuild(guildID: string): Promise<void>;
     leaveVoiceChannel(channelID: string): void;
@@ -2593,7 +2593,7 @@ declare namespace Eris {
     voiceMembers: Collection<Member>;
     createInvite(options?: CreateInviteOptions, reason?: string): Promise<Invite<"withMetadata", VoiceChannel>>;
     getInvites(): Promise<(Invite<"withMetadata", VoiceChannel>)[]>;
-    join(options: { opusOnly?: boolean; shared?: boolean }): Promise<VoiceConnection>;
+    join(options: { opusOnly?: boolean; shared?: boolean, selfMute?: boolean, selfDeaf?: boolean }): Promise<VoiceConnection>;
     leave(): void;
   }
 
