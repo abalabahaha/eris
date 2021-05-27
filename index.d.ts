d1186d4701c737d7c65008e682ede42d8c56700c
Bsian <chharry321@gmail.com>
2021-05-27 10:37:01 +0100

    Expose Message#applicationID

--- index.d.ts
+++ index.d.ts
@@ -2345,6 +2345,7 @@ declare namespace Eris {
   export class Message<T extends PossiblyUncachedTextable = TextableChannel> extends Base {
     activity?: MessageActivity;
     application?: MessageApplication;
+    applicationID?: string;
     attachments: Attachment[];
     author: User;
     channel: T;
