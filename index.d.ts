2007c40d42fa56fd8a357c9083bba358b2444d59
Bsian <chharry321@gmail.com>
2021-06-09 11:47:22 +0100

    Allow passing a plain object to permissionsOf

--- index.d.ts
+++ index.d.ts
@@ -796,6 +796,9 @@ declare namespace Eris {
     id: string;
     user: User;
   }
+  interface MemberRoles extends BaseData {
+    roles: string;
+  }
   interface PartialUser {
     avatar: string | null;
     discriminator: string;
@@ -2044,7 +2047,7 @@ declare namespace Eris {
     kickMember(userID: string, reason?: string): Promise<void>;
     leave(): Promise<void>;
     leaveVoiceChannel(): void;
-    permissionsOf(memberID: string | Member): Permission;
+    permissionsOf(memberID: string | Member | MemberRoles): Permission;
     pruneMembers(options?: PruneMemberOptions): Promise<number>;
     removeMemberRole(memberID: string, roleID: string, reason?: string): Promise<void>;
     searchMembers(query: string, limit?: number): Promise<Member[]>;
@@ -2094,7 +2097,7 @@ declare namespace Eris {
     ): Promise<PermissionOverwrite>;
     editPosition(position: number, options?: EditChannelPositionOptions): Promise<void>;
     getInvites(): Promise<Invite[]>;
-    permissionsOf(memberID: string | Member): Permission;
+    permissionsOf(memberID: string | Member | MemberRoles): Permission;
   }
 
   export class GuildIntegration extends Base {
