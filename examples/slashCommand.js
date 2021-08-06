c933f965adcb01763c4e3f69e93adf85699f1591
Navy Cake <navycakedeveloper@gmail.com>
2021-08-06 17:02:50 -0300

    Add support for adding files in message editing. And updating WebSocket.

--- examples/slashCommand.js
+++ examples/slashCommand.js
@@ -9,48 +9,47 @@ const bot = new Eris.CommandClient('Bot TOKEN', {}, {
 
 
 bot.on('ready', async () => { // When the bot is ready
-    console.log('Ready!'); // Log "Ready!"
+  console.log('Ready!'); // Log "Ready!"
 
-    // First we have to load the command list.
+  // First we have to load the command list.
 
   await client.slashCommand.loadCommandList();
 
 
-    // Let's create commands globally.
+  // Let's create commands globally.
   await client.slashCommand.createCommand(
     new CommandBase()
-          .setName('blep')
-          .setDescription("Send a random adorable animal photo")
-          .addOptions(        
-              new CommandOptions()                 
-                  .setName('animal')
-                  .setDescription('The type of animal')
-                  .setType(3)                
-                  .addChoices(
-                      new Choice()                        
-                          .setName('Dog')                        
-                          .setValue('animal_dog'),                      
-                      new Choice()               
-                          .setName('Cat')                         
-                          .setValue('animal_cat'),                      
-                      new Choice()               
-                          .setName('Penguin')                         
-                          .setValue('animal_penguin'), 
-              ) 
-                  .isRequired(),
-              
-        
+      .setName('blep')
+      .setDescription('Send a random adorable animal photo')
+      .addOptions(
+        new CommandOptions()
+          .setName('animal')
+          .setDescription('The type of animal')
+          .setType(3)
+          .addChoices(
+            new Choice()
+              .setName('Dog')
+              .setValue('animal_dog'),
+            new Choice()
+              .setName('Cat')
+              .setValue('animal_cat'),
+            new Choice()
+              .setName('Penguin')
+              .setValue('animal_penguin')
+          )
+          .isRequired(),
+
+
         // Other options ^^
-              new CommandOptions()          
-                  .setName('only_smol')          
-                  .setDescription('Whether to show only baby animals')            
-                  .setType(5)          
-                  .isRequired(),
-              
-    )
-  )
-    
-    // Ready. Now you can see in Discord if the commands were loaded.
+        new CommandOptions()
+          .setName('only_smol')
+          .setDescription('Whether to show only baby animals')
+          .setType(5)
+          .isRequired()
+      )
+  );
+
+  // Ready. Now you can see in Discord if the commands were loaded.
 });
 
 bot.connect();
