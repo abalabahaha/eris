3f138f4991eac83d6082b2721a3b4d700b10e878
bsian03 <chharry321@gmail.com>
2021-10-16 15:15:52 +0100

    Correct event types

--- index.d.ts
+++ index.d.ts
@@ -541,6 +541,7 @@ declare namespace Eris {
     relationshipAdd: [relationship: Relationship];
     relationshipRemove: [relationship: Relationship];
     relationshipUpdate: [relationship: Relationship, oldRelationship: { type: number }];
+    shardPreReady: [id: number];
     typingStart: [channel: GuildTextableChannel | Uncached, user: User | Uncached, member: Member] 
       | [channel: PrivateChannel | Uncached, user: User | Uncached, member: null];
     unavailableGuildCreate: [guild: UnavailableGuild];
@@ -572,7 +573,7 @@ declare namespace Eris {
     disconnect: [err?: Error];
     end: [];
     error: [err: Error];
-    ping: [latency: number];
+    pong: [latency: number];
     ready: [];
     speakingStart: [userID: string];
     speakingStop: [userID: string];
