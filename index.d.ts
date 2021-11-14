0ae09f1272ef6b6e6ef70ffb8f1a61ea38b5db99
abalabahaha <hi@abal.moe>
2021-11-14 08:22:08 +0000

    Add Intent groups, default to non-privileged intents

--- index.d.ts
+++ index.d.ts
@@ -1510,6 +1510,9 @@ declare namespace Eris {
       directMessages: 4096;
       directMessageReactions: 8192;
       directMessageTyping: 16384;
+      allNonPrivileged: 32509;
+      allPrivileged: 258;
+      all: 32767;
     };
     MessageActivityFlags: {
       INSTANCE: 1;
