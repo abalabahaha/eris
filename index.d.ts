791be08283363ee14938420ca2174448db33afec
Donovan Daniels <hewwo@yiff.rocks>
2022-01-17 17:28:01 -0600

    `default_permission` -> `defaultPermission` (#1325)

--- index.d.ts
+++ index.d.ts
@@ -200,7 +200,7 @@ declare namespace Eris {
     description: T extends Constants["ApplicationCommandTypes"]["CHAT_INPUT"] ? string : never;
     options?: ApplicationCommandOption[];
     type: T;
-    default_permission?: boolean;
+    defaultPermission?: boolean;
   }
 
   type AnyApplicationCommand = ChatInputApplicationCommand | MessageApplicationCommand | UserApplicationCommand;
