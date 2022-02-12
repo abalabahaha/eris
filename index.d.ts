910cde7fdf57d29932ceee8490f517e79247c712
Linker-123 <Linker-123@users.noreply.github.com>
2022-02-12 14:04:39 +0100

    Narrow down the component types in ModalSubmitInteractionData

--- index.d.ts
+++ index.d.ts
@@ -3001,9 +3001,13 @@ declare namespace Eris {
     result(choices: ApplicationCommandOptionChoice[]): Promise<void>;
   }
 
+  interface ModalSubmitInteractionDataComponents {
+    components: TextInput[];
+    type: Constants["ComponentTypes"]["ACTION_ROW"];
+  }
   interface ModalSubmitInteractionData {
     custom_id: string;
-    components: ActionRow[];
+    components: ModalSubmitInteractionDataComponents[];
   }
 
   export class ModalSubmitInteraction<T extends PossiblyUncachedTextable = TextableChannel> extends Interaction {
