ef2891f1361b1abd23b8d47c88fd063887f5c5bd
Donovan Daniels <hewwo@yiff.rocks>
2022-08-18 04:55:41 -0500

    Remove new select type names

--- index.d.ts
+++ index.d.ts
@@ -1656,14 +1656,7 @@ declare namespace Eris {
     ComponentTypes: {
       ACTION_ROW:         1;
       BUTTON:             2;
-      /** @deprecated */
       SELECT_MENU:        3;
-      STRING_SELECT:      3;
-      INPUT_TEXT:         4;
-      USER_SELECT:        5;
-      ROLE_SELECT:        6;
-      MENTIONABLE_SELECT: 7;
-      CHANNEL_SELECT:     8;
     };
     ConnectionVisibilityTypes: {
       NONE:     0;
