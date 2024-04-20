f580224b2cede92cf3d1ba60d9e41bc4d77c8d88
Conor™ <conor.turner11@gmail.com>
2024-04-20 22:55:35 +0100

    Fix

--- index.d.ts
+++ index.d.ts
@@ -2429,14 +2429,14 @@ declare namespace Eris {
       NITRO:         2;
     };
     RoleConnectionMetadataTypes: {
-      INTEGER_LESS_THAN_OR_EQUAL:     1,
-      INTEGER_GREATER_THAN_OR_EQUAL:  2,
-      INTEGER_EQUAL:                  3,
-      INTEGER_NOT_EQUAL:              4,
-      DATETIME_LESS_THAN_OR_EQUAL:    5,
-      DATETIME_GREATER_THAN_OR_EQUAL: 6,
-      BOOLEAN_EQUAL:                  7,
-      BOOLEAN_NOT_EQUAL:              8
+      INTEGER_LESS_THAN_OR_EQUAL:     1;
+      INTEGER_GREATER_THAN_OR_EQUAL:  2;
+      INTEGER_EQUAL:                  3;
+      INTEGER_NOT_EQUAL:              4;
+      DATETIME_LESS_THAN_OR_EQUAL:    5;
+      DATETIME_GREATER_THAN_OR_EQUAL: 6;
+      BOOLEAN_EQUAL:                  7;
+      BOOLEAN_NOT_EQUAL:              8;
     };
     RoleFlags: {
       IN_PROMPT: 1;
