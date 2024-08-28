880483d0d9fe2c3628dcec9861e1e04d19f22786
Areo <mrareo@proton.me>
2024-08-28 22:42:04 +0000

    typings: Add banner to EditSelfOptions (#1533)

--- index.d.ts
+++ index.d.ts
@@ -552,6 +552,7 @@ declare namespace Eris {
   }
   interface EditSelfOptions {
     avatar?: string | null;
+    banner?: string | null;
     username?: string;
   }
   interface RequestHandlerOptions {
@@ -2837,7 +2838,7 @@ declare namespace Eris {
     editRole(guildID: string, roleID: string, options: RoleOptions, reason?: string): Promise<Role>; // TODO not all options are available?
     editRoleConnectionMetadataRecords(data: ApplicationRoleConnectionMetadata[]): Promise<ApplicationRoleConnectionMetadata[]>;
     editRolePosition(guildID: string, roleID: string, position: number): Promise<void>;
-    editSelf(options: { avatar?: string; username?: string }): Promise<ExtendedUser>;
+    editSelf(options: EditSelfOptions): Promise<ExtendedUser>;
     editStageInstance(channelID: string, options: StageInstanceOptions): Promise<StageInstance>;
     editStatus(status: SelfStatus, activities?: ActivityPartial<ActivityType>[] | ActivityPartial<ActivityType>): void;
     editStatus(activities?: ActivityPartial<ActivityType>[] | ActivityPartial<ActivityType>): void;
