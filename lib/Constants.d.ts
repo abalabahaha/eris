5ef8a45c9286985103a41d9c53ba94ddf2a101fc
seeyebe <dawsha.upp@gmail.com>
2025-08-05 12:07:35 +0300

    feat: add Components V2 constants

--- lib/Constants.d.ts
+++ lib/Constants.d.ts
@@ -182,6 +182,13 @@ export default interface Constants {
     ROLE_SELECT:        6;
     MENTIONABLE_SELECT: 7;
     CHANNEL_SELECT:     8;
+    SECTION:            9;
+    TEXT_DISPLAY:       10;
+    THUMBNAIL:          11;
+    MEDIA_GALLERY:      12;
+    FILE:               13;
+    SEPARATOR:          14;
+    CONTAINER:          17;
   };
   ForumLayoutTypes: {
     NOT_SET:      0;
@@ -430,6 +437,7 @@ export default interface Constants {
     FAILED_TO_MENTION_SOME_ROLES_IN_THREAD: 256;
     SUPPRESS_NOTIFICATIONS:                 4096;
     IS_VOICE_MESSAGE:                       8192;
+    IS_COMPONENTS_V2:                       32768;
   };
   MessageTypes: {
     DEFAULT:                                      0;
