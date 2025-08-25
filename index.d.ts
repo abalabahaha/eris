061f3430612685164f4a08d8f69570099e18b5e3
Conor™ <conor.turner11@gmail.com>
2025-08-25 10:53:37 +0100

    ts

--- index.d.ts
+++ index.d.ts
@@ -859,6 +859,7 @@ declare namespace Eris {
   }
   interface OldRole {
     color: number;
+    colors: RoleColors;
     flags: number;
     hoist: boolean;
     icon: string | null;
@@ -1805,8 +1806,14 @@ declare namespace Eris {
     permissions?: number;
     position?: number;
   }
+  interface RoleColors {
+    primaryColor: number;
+    secondaryColor?: number | null;
+    tertiaryColor?: number | null;
+  }
   interface RoleOptions {
     color?: number;
+    colors?: RoleColors;
     hoist?: boolean;
     icon?: string;
     mentionable?: boolean;
@@ -3268,6 +3275,7 @@ declare namespace Eris {
 
   export class Role extends Base {
     color: number;
+    colors: RoleColors;
     createdAt: number;
     flags: number;
     guild: Guild;
